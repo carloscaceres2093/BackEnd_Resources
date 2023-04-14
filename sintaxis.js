438b6f47006dec2c00e9cf657d357ce733569254
carloscaceres2093 <carlosacr2093@gmail.com>
2023-04-13 21:55:49 -0500

    Add JS Basics

---
+++ sintaxis.js
@@ -0,0 +1,124 @@
+// Declaración
+// console.log("============================ DECLARACIÓN ============================")
+// const constante = "Ejemplo COnstante"
+// let variable1 = 3
+// var variable2 = 2
+// var variable2 = 3;
+// console.log(variable2)
+// {
+//   let varaibleTest1 = 1;
+//   var varaibleTest12 = "Esto es una prueba"
+//   let variable2 = "Texto";
+//   let variable1 = 1;
+//   console.log(variable2)
+// }
+
+// console.log(variable2)
+
+// console.log("============================ OPERADORES ============================")
+// let increment = 8 
+// let decrement = 8 
+// SUma
+// console.log(1+2)
+// Resto
+// console.log(3-2)
+// Multiplicación
+// console.log(3*2)
+// Divsión
+// console.log(8/2)
+// Potenciación
+// console.log(2**3)
+// Módulo
+// console.log(7%2)
+// increment++ // -> increment = increment+1
+// Incremento
+// console.log(increment)
+// decrement-- // -> decrement = decrement-1
+// Decremento
+// console.log(decrement)
+
+// let x = 3;
+// const y = x++; //x++ -> x = x+1
+
+// console.log("esto es x: ", x, "esto es y: ",y)
+// let a = 3;
+// const b = ++a
+// console.log("esto es a: ", a, "esto es b: ",b)
+
+// let n = 3;
+// const m = n+1
+
+// {
+//   let n = 5 
+
+//   n = "yo"
+// }
+// n = 1
+// let o = 5
+// o = 3
+// var v = 1
+// var v = 2
+// v = 2
+
+// console.log("esto es m: ", m, "esto es n: ",n)
+
+// console.log("============================ Asignación ============================")
+// // Asiganación
+// let n = 5
+// // Abreviación
+// // Suma
+// n += 8 // n = n + 8
+// // Resta
+// n -= 8 // n = n - 8
+// // Multi
+// n *= 8 // n = n * 8
+// // Division
+// n /= 8 // n = n / 8
+// // Modulo
+// n %= 2 // n = n % 2
+// // Potencia
+// n **= 2 // n = n ** 2
+
+console.log("============================ Tipos De datos ============================")
+
+// String
+var x = "String"
+console.log(x)
+
+// Number
+x = 16
+console.log(x)
+x = 16.5
+console.log(x)
+// Bigint
+x = 16
+console.log(x)
+// Boolean
+x = true
+console.log(x)
+// Undefined
+let ud 
+console.log(ud)
+ud = undefined
+// Null
+x = ""
+console.log(x)
+x = null
+console.log(x)
+
+// Object
+
+// Object
+const persona = {nombre:"John", edad:8, apellido:"Manizales"}
+console.log(persona)
+console.log("Tipo Dato:", typeof persona)
+// Array
+const marcasCarros = ["Volvo", "BMW", "Mercedes", 8]
+console.log(marcasCarros)
+console.log("Tipo Dato:", typeof marcasCarros)
+// Date
+const dateE =  new Date()
+console.log(dateE)
+const dateEA =  new Date("2023-04-03")
+console.log(dateEA)
+console.log("Tipo Dato:", typeof dateEA)
