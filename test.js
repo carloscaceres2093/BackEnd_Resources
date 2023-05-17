54e41fbfbc6d90415ae7878f11d999dea2543e18
carloscaceres2093 <carlosacr2093@gmail.com>
2023-05-16 20:17:54 -0500

    typescript modules

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 3000;
 const server = http.createServer((req, res) => {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end("Hello This is LinuxHint! and I am Sam\n");
+	res.end("Hello This is BackEnd Course! and I am Carlos\n");
 });
 server.listen(port, () => {
 	console.log(`Listening Port ${port}`);
